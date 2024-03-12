bf25c7dc651cad923d8b59786ae40fcc54b8ce51
Shubhangi Raj <shubhangi120989@gmail.com>
2024-03-12 13:34:08 +0530

    Initial Commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
 const app = express();
-config({ path: "./config/config.env" });
+config({ path: ".env" });
 
 app.use(
   cors({
